51e556d33b7912169d94bb8d7395608fbbe1eac1
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-17 04:05:02 +0700

    feat: menambahkan link pada logo astro

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,13 @@ import { SiAstro } from 'react-icons/si';
 const Footer = () => {
   return (
     <footer>
-      <div className="my-4 flex h-24 flex-col items-center justify-center gap-2 text-gray-800">
+      <div className="my-4 flex h-24 flex-col items-center justify-center gap-2 text-gray-600">
         <div className="flex gap-2">
           <p>Powered by</p>
-          <SiAstro size={25} />
+          <a href="https://astro.build/" className="hover:text-gray-800">
+            <title>Astro</title>
+            <SiAstro size={25} />
+          </a>
         </div>
         <p>Copyright© {new Date().getFullYear()}.</p>
       </div>
