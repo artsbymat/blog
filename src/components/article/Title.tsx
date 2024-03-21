c21496ead4597711385b6c85fe6ddd616ecadc30
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-21 07:05:28 +0700

    feat: membuat halaman post dynamic

---
+++ src/components/article/Title.tsx
@@ -0,0 +1,9 @@
+const Title = ({ title }) => {
+  return (
+    <div className="my-10 px-4">
+      <p className="sm:text-4xl text-3xl text-center font-medium">{title}</p>
+    </div>
+  );
+};
+
+export default Title;
