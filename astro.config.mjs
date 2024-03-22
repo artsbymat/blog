810416fa4b41de92b5a0cfcac2f4e49f73493343
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-22 16:52:25 +0700

    feat: menambahkan tanda external link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
@@ -13,7 +14,15 @@ export default defineConfig({
       langs: ['html', 'css', 'js', 'ts', 'jsx', 'tsx'],
       wrap: false,
       transformers: []
-    }
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: 'text', value: '⤴' }
+        }
+      ]
+    ]
   },
   image: {
     domains: ['res.cloudinary.com']
