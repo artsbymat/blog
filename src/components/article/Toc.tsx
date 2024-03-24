44f28aa336e73a7ebe934a3a7f1ffb7d1e044c82
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-25 01:28:58 +0700

    feat: menambahkan element daftar isi

---
+++ src/components/article/Toc.tsx
@@ -0,0 +1,62 @@
+const Toc = ({ headings }) => {
+  function buildHierarchy(headings) {
+    const toc = [];
+    const parentHeadings = new Map();
+
+    if (!headings) return toc;
+
+    headings.forEach((h) => {
+      const heading = { ...h, subheadings: [] };
+
+      if (heading.text === 'Footnotes') return;
+      if (heading.depth === 1) {
+        toc.push(heading);
+      } else if (heading.depth === 2) {
+        const parentHeading = parentHeadings.get(heading.depth - 1);
+        if (parentHeading) {
+          parentHeading.subheadings.push(heading);
+        }
+      } else if (heading.depth === 3) {
+        const parentHeading = parentHeadings.get(heading.depth - 2);
+        if (parentHeading) {
+          parentHeading.subheadings.push(heading);
+        }
+      }
+
+      parentHeadings.set(heading.depth, heading);
+    });
+    return toc;
+  }
+  return (
+    <div className="px-4 my-8">
+      <div className="p-2 border-gray-500 border-2 rounded-md">
+        <div className="p-2">
+          <p className="text-2xl font-medium text-gray-600">Table Of Contents</p>
+        </div>
+        <ol>
+          {buildHierarchy(headings).map((h) => (
+            <li key={h.slug} className="ml-8 text-[1.1rem] text-blue-500 list-decimal">
+              <a href={`#${h.slug}`} className="hover:underline">
+                {h.text}
+              </a>
+              {h.subheadings.length > 0 && (
+                <ul className="">
+                  {h.subheadings.map((sh) => (
+                    <li
+                      key={sh.slug}
+                      className={`hover:text-blue-700 list-disc ${sh.depth === 2 && 'ml-6'} ${sh.depth === 3 && 'ml-12'}`}
+                    >
+                      <a href={`#${sh.slug}`}>{sh.text}</a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Toc;
