cca2dbfe26651736ca2e1c549d9bec26f998160c
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-03-26 08:09:03 +0700

    fix: memperbaiki tag yang duplikat

--- src/components/article/Tags.tsx
+++ src/components/article/Tags.tsx
@@ -1,10 +1,11 @@
 const Tags = ({ tags }) => {
+  const uniqueTags = [...new Set(tags.map((tag) => tag).flat())];
   return (
     <div className="px-4">
       <div className="pt-4 border-t-2 flex items-center text-gray-700">
         <p>Tags: </p>
         <ul className="flex text-lg ml-2 items-center">
-          {tags.map((tag, index) => (
+          {uniqueTags.map((tag, index) => (
             <li>
               {index !== 0 && ', '}
               <a href={`/tags/${tag}`}>{tag}</a>
